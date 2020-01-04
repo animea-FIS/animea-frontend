9a3d5f360fc08ee13c02a6727b501d059293e883
camreyaro <camreyaro@alum.us.es>
2020-01-04 14:23:38 +0100

    unused imports removed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  withRouter
-} from "react-router-dom";
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
-import Animes from './components/anime/Animes';
-import AnimeInfo from './components/anime/AnimeInfo';
-import NotFound from './components/common/NotFound';
 import SideNav from './components/common/SideNav';
 
-function App() {
+function App() { 
   return (
     <SideNav />
   );
