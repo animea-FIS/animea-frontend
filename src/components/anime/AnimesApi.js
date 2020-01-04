42e889d940d673c5aa19af193ee1bc8a66ac23b2
camreyaro <camreyaro@alum.us.es>
2020-01-04 21:11:58 +0100

    changed http to https in animes api

--- src/components/anime/AnimesApi.js
+++ src/components/anime/AnimesApi.js
@@ -1,5 +1,5 @@
 class AnimesApi {
-    static API_BASE_URL = `http://185.176.5.147:7400/animes/api/v1`
+    static API_BASE_URL = `https://${REACT_APP_SERVER_IP}:${REACT_APP_GATEWAY_PORT}/animes/api/v1`
 
     static requestHeaders() {
         return {}
