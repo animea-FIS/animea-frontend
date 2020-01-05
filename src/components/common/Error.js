daee408292f3c60bf647852eab5c740218642e32
camreyaro <camreyaro@alum.us.es>
2020-01-05 17:32:21 +0100

    error page modified

--- src/components/common/Error.js
+++ src/components/common/Error.js
@@ -3,15 +3,23 @@ import { withRouter } from "react-router-dom";
 
 class Error extends React.Component {
     render() {
+      var errorCode;
+      var errorMsg;
         if (this.props.location.state && this.props.location.state.errorCode){
-        return (
+          errorCode = this.props.location.state 
+          errorMsg = this.props.location.state.errorCode
+        } else {
+          errorCode = 500
+          errorMsg = 'Something went wrong'
+        }
+       return (
           <div className="container">
             <div class="row">
             <div class="col s12">
               <div class="card red darken-1">
                 <div class="card-content white-text center-block">
-                  <span class="card-title center"><h4>{this.props.location.state.errorCode} Error</h4></span>
-                  <p class="center"><h4>{this.props.location.state.errorMessage}</h4></p>
+                  <span class="card-title center"><h4>{errorCode} Error</h4></span>
+                  <p class="center"><h4>{errorMsg}</h4></p>
                   <img src=""/>
                 </div>
               </div>
@@ -19,9 +27,6 @@ class Error extends React.Component {
           </div>
           </div>
         );
-        } else{
-          return <div></div>
-        }
     }
 }
 
